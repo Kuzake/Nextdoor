55f57d0d925e5efcc07d63f76a6e4d06e300ec9a
Joris <reixjoris@gmail.com>
2016-12-01 18:34:23 +0100

    Profil privé

--- js/custom.js
+++ js/custom.js
@@ -3,4 +3,5 @@ $(document).ready(function(){
   $(".button-collapse").sideNav(); // Menu mobile
   $('.modal').modal(); // Modal connexion
   $('.parallax').parallax(); // Parallax
+  $('#msg').trigger('autoresize'); // Messages privés
 });
